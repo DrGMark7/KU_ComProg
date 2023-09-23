cea0d4f78f9cd58385ac72e370e625d08c46497c
DrGMark7 <72572485+DrGMark7@users.noreply.github.com>
2023-09-23 18:41:47 +0700

    Update 01 Matrix Plus.py

---
+++ Elab11/01 Matrix Plus.py
@@ -0,0 +1,17 @@
+def plus_matrix(A,B):
+    new_matrix = []
+    for row1,row2 in zip(A,B):
+        row = []
+        for i,j in zip(row1,row2):
+            row.append(i+j)
+        new_matrix.append(row)
+
+    return new_matrix
+
+def print_matrix(A):
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            print(f'{A[i][j]:^6}', end = ' ')
+        print()
+
+print_matrix(plus_matrix(A,B))
